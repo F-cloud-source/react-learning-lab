3a9ec3f9d87b79a8b942bca8250311e531f8794c
Faizan Ali <fizcq354@outlook.com>
2025-12-06 16:02:22 +0500

    cut all code

--- my-react-App/src/App.jsx
+++ my-react-App/src/App.jsx
@@ -1,21 +1,12 @@
 // Import from Header and Footer  component in App.jsx for React Project
 
-import Header from './Header'
-import Footer from './Footer'
-import List from './List'
+// import Header from './Header'
+ // import Footer from './Footer'
+ //  import List from './List'
 
 function App() {
   
-   return(
-    <>
-    <Header></Header>
-    <Footer></Footer>
-    <List></List>
-    <Header></Header>
-    <List></List>
 
-    </>
-   );
 }
 
 export default App;

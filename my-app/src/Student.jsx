9310606c6dbf6acc8cd35d1826b31400d57525f1
Faizan Ali <fizcq354@outlook.com>
2025-12-21 14:25:13 +0500

    Built New props Component

--- my-app/src/Student.jsx
+++ my-app/src/Student.jsx
@@ -1,12 +1,17 @@
+// PropTypes = a mechanism ensures the value is passed
+//              as props is correct datatype
+//              Age = PropTypes.number
+
 
 
 function Student(props) {
       return(
-        <div>
+        <div className="student-1">
             <h1>Name: {props.name}</h1>
             <h1>Date Of Birth: {props.birthDate}</h1>
             <h1>Age: {props.age}</h1>
-            { <h1>Stable Job: {props.job ? "Yes" : "NO" }</h1> }
+            <h2>Salary in Dollars:{props.salary}</h2>
+            <h1>Stable Job: {props.job === "Yes" ? "Yes" : "No"}</h1>
 
         </div>
       )

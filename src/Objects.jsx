c788417116ebdf5b3b701a4469bf47aa91331acd
Faizan Ali <fizcq354@outlook.com>
2026-01-31 12:41:57 +0500

    Car and User Detail Object Components

--- src/Objects.jsx
+++ src/Objects.jsx
@@ -4,41 +4,101 @@ import React, {useState} from 'react'
 
 function Object(){
 
-// intail state of React Car Objects 
+// intial state of React  Component Based Design 
+// for Car Object(Including all Object details ) 
+//  IT is based on prop  and ke based Design in 
+// React.Js
+// Use Object Rendering to built Car and UserName Components Design
+
     const [ car , setCar] = useState({ year: 2024,
                                        model: "Toyota Yaris",
                                        make: "Toyota",
-                                        });
+                                       quality: "S-class",
+                                       export : "German"        
+                                     });
+    
+    const [userName , SetUsername] = useState({
+                                    name: "Alex", 
+                                    emailId: "alex123@gmail.com",
+                                    MarriedStatus: "yes",
+                                    });                       
 
-
-   
-
-
+  // Year Onchange event After UI  renderings  Use Updater Fucntion for better quality and Performance.
 function HanldeYearChange(event){
 
 
     setCar( c => ({... c, year: event.target.value}));
-}         
+};       
 
 
-
+ // Make Onchange event After UI  renderings  Use Updater Fucntion for better quality and Performance.
 function HanldeMakeChange(event){
      
     setCar( c => ({... c,  make: event.target.value}));
-} 
+}; 
 
 
+ // Model Onchange event After UI  renderings  Use Updater Fucntion for better quality and Performance.
 function HanldeModelChange(event){
       
-setCar( c => ({... c,  model: event.target.value}));
-   
-} 
+setCar( c => ({... c,  model: event.target.value}));  
+                                     
+};
+
+
+ // Quality Onchange event After UI  renderings  Use Updater Fucntion for better quality and Performance.
+function HanldeQualityChange(event){
+
+
+    setCar( c => ({... c, quality: event.target.value}));
+};       
+
+
+ // Export Onchange event After UI  renderings  Use Updater Fucntion for better quality and Performance.
+function HanldleExportChange(event){
+
+
+    setCar( c => ({... c, export: event.target.value}));
+};       
+
+
+function HandleNameChange(event){
+ 
+    SetUsername( c => ({... c , name: event.target.value}))
+
+};
+
+
+function HandleChange(event){
+ 
+    SetUsername( c => ({... c , name: event.target.value}))
+
+};
+
+
+function HandleEmailIDChange(event){
+ 
+    SetUsername( c => ({... c , emailId: event.target.value}))
+
+};
+
+
+function HandleMartialStatusChange(event){
+ 
+    SetUsername( c => ({... c , arriedStatus: event.target.value}))
+
+};
   return(
     <div>
         <h1>Your liking Car: {car.model}  {car.year} {car.make} </h1>
         <input type="number" value={car.year} onChange={HanldeYearChange}/>
         <input type="text"  value={car.make}  onChange={HanldeMakeChange}/>
         <input type="text"  value={car.model}  onChange={HanldeModelChange}/>
+        <input type="text"  value={car.quality}  onchange={HanldeQualityChange}/>
+        <input type="text"  value={car.export}  onchange={HanldleExportChange}/>
+        <input type="text"  value={userName.name}  onchange={HandleNameChange}/>
+        <input type="text"  value={userName.emailId}  onchange={HandleEmailIDChange}/>
+        <input type="text"  value={userName.MarriedStatus}  onchange={HandleMartialStatusChange}/>
        
     </div>
   )
